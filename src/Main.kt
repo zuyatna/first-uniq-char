fd5727ce4e96542bca252f96851db53020d356af
Suyatna <zuyatna@gmail.com>
2024-07-04 14:17:15 +0700

    first approach (but has time limit exceeded)

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    firstUniqChar("leetcode")
+    firstUniqChar("loveleetcode")
 }
 
 fun firstUniqChar(s: String): Int {
-    val breakChar = s.map { it.toString() }.toTypedArray().toMutableList()
-    println(breakChar.toList())
+    val breakChar = s.map { it.toString() }.toTypedArray()
 
-    return 0
+    for (i in breakChar.indices) {
+        val copyBreakChar = breakChar.toMutableList()
+        copyBreakChar.removeAt(i)
+        println("index: $i, value: ${breakChar[i]}")
+        println(copyBreakChar.toList())
+
+        if (copyBreakChar.contains(breakChar[i])) {
+            println("contain")
+            println()
+        } else {
+            println("not contain")
+            return i
+        }
+    }
+
+    return -1
 }
